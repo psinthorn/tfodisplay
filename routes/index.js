4d52ab92dd468ac22e2c1a56be0ce21e42b2bb40
Sinthorn Pradutnam <psinthorn@gmail.com>
2018-12-20 16:00:15 +0700

    text index update

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,29 @@
-const IndexController = require('./../controllers/IndexController');
-const WelcomeController = require('./../controllers/admin/welcomeController');
-const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
+const IndexController = require("./../controllers/IndexController");
+const WelcomeController = require("./../controllers/admin/welcomeController");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
-
-module.exports = (app) => {
-
-   //Welcome landing page
-  app.get('/', IndexController.index);
-   app.get("/text2", IndexController.indexText);
-  app.get('/banner', IndexController.banner);
- 
+module.exports = app => {
+  //Welcome landing page
+  app.get("/", IndexController.index);
+  app.get("/display/1", IndexController.indexText);
+  app.get("/banner", IndexController.banner);
 
   //Company Profile Page
-  app.get('/company-profile', IndexController.companyProfile);  
+  app.get("/company-profile", IndexController.companyProfile);
 
   //about page
-  app.get('/about', IndexController.about);  
-  
+  app.get("/about", IndexController.about);
 
   //Service list
-  app.get('/services', IndexController.services );
-  app.get('/service-show/:id', IndexController.serviceShow);
-
+  app.get("/services", IndexController.services);
+  app.get("/service-show/:id", IndexController.serviceShow);
 
   //transfers booking form
-  app.get('/term-policies', IndexController.termPolicy );
-
+  app.get("/term-policies", IndexController.termPolicy);
 
   //Contact page
-  app.get('/contact-us', IndexController.contact);
+  app.get("/contact-us", IndexController.contact);
 
   //Maps
-  app.get('/maps', IndexController.maps);
-
-}
+  app.get("/maps", IndexController.maps);
+};
